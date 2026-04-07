e5bc94b5c095f0ef2d7b9b50af31c4b5423d39c4
mocbaltig <lekjonde@gmail.com>
2026-04-07 19:30:34 +0700

    feat: add dotenv package

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import server from './server/index.js';
 
-const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
-const port = process.env.PORT || 3000;
+const host = process.env.HOST;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
