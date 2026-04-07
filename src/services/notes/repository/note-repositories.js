0badb9296db090861437a4f3ee33c3263e093c8b
mocbaltig <lekjonde@gmail.com>
2026-04-07 20:23:21 +0700

    feat(NoteRepositories): complete other CRUD function

--- src/services/notes/repository/note-repositories.js
+++ src/services/notes/repository/note-repositories.js
@@ -19,6 +19,41 @@ class NoteRepositories {
     const result = await this.pool.query(query);
     return result.rows[0];
   }
+
+  async getNotes() {
+    const result = await this.pool.query('SELECT * FROM notes');
+    return result.rows;
+  }
+
+  async getNoteById(id) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+
+  async editNoteById({ id, title, body, tags }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [title, body, tags, updatedAt, id],
+    };
+
+    const result = await this.pool.query(query);
+    return result.rows[0];
+  }
+
+  async deleteNoteById(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    return result.rows[0].id;
+  }
 }
 
 export default NoteRepositories;
