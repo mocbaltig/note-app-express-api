15372344d350a7472963dcb5d4e27665d48b710f
mocbaltig <lekjonde@gmail.com>
2026-04-12 19:49:10 +0700

    feat(authentications): add authentication-controller & token-manager

---
+++ src/security/token-manager.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { InvariantError } from '../exceptions/index.js';
+
+const TokenManager = {
+  generateAccessToken: (payload) =>
+    jwt.sign(payload, process.env.ACCESS_TOKEN_KEY),
+  generateRefreshToken: (payload) =>
+    jwt.sign(payload, process.env.REFRESH_TOKEN_KEY),
+  verifyRefreshToken: (refreshToken) => {
+    try {
+      const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY);
+      return payload;
+    } catch (err) {
+      console.log(err);
+      throw new InvariantError('Refresh token tidak valid');
+    }
+  },
+};
+
+export default TokenManager;
